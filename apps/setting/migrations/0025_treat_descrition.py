8a7acef8d77edf4f603b9e642cd88183a22a1a9f
Kulmamatov Zalkar <kulmamatov.zalkar@gmail.com>
2025-07-31 20:23:53 +0600

    test

--- apps/setting/migrations/0025_treat_descrition.py
+++ apps/setting/migrations/0025_treat_descrition.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(
             model_name='treat',
-            name='descrition',
+            name='description',
             field=models.TextField(blank=True, null=True),
         ),
     ]
